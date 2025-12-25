9e8922fae0d5ad33e27aa1d9b209bdbab12344ac
Omar Hashad <omarhashad88@gmail.com>
2025-12-25 18:51:39 +0200

    refactor: improve token retrieval logic in getToken function

--- src/lib/getToken.ts
+++ src/lib/getToken.ts
@@ -6,8 +6,8 @@ import { cookies } from "next/headers";
 export const getToken = async function () {
   const cookieName =
     process.env.NODE_ENV === "production"
-      ? "__Secure-next-auth.session.token"
-      : "next-auth.session.token";
+      ? "__Secure-next-auth.session-token"
+      : "next-auth.session-token";
   const authToken = (await cookies()).get(cookieName)?.value;
 
   if (!authToken) {
